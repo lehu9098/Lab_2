1935a3a5b27e508b6198fe748e6879241b45b24b
Leo Huettel <leohuettel@engr2gw-65-132-dhcp.int.colorado.edu>
2019-01-30 13:35:03 -0700

    lab2 solution

---
+++ lab2_scripts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Authors : leo
+# Date: 1/30/2019
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+echo "reg exp"
+read
+grep -c '{0-9}\-{0-9}\{3\}\-{0-9}\{4\}' regex_practice.txt
+
