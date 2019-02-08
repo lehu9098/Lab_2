56f8f979becf7a66f1990985895e6cf0260cf27d
Leo Huettel <leohuettel@cu-engr2-1-13-10.201.48.139.int.colorado.edu>
2019-02-08 13:58:52 -0700

    updated scripts

--- lab2_scripts.sh
+++ lab2_scripts.sh
@@ -4,7 +4,18 @@
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
-echo "reg exp"
-read
-grep -c '{0-9}\-{0-9}\{3\}\-{0-9}\{4\}' regex_practice.txt
+echo "Enter a regex:"
+read regexp_var
+echo "enter a filename:"
+read filename
 
+## Do grep of regex_expr on filename
+grep -E $regexp_var $filename 
+
+grep -cE '[0-9]{3}-[0-9]{3}-[0-9]{4}' regex_practice.txt
+#read numOfphone
+grep -cE ".*@[A-Za-z].[A-Za-z]*" regex_practice.txt
+#read numOfemail
+grep -E "(303)-[0-9]{3}-[0-9]{4}" regex_practice.txt > phone_results.txt
+grep -E ".*@(geocities\.com)" regex_practice.txt > email_results.txt
+grep -E $1 regex_practice.txt > command_results.txt
